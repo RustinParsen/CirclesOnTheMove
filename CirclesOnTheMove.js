2109e417d6014e5161d05cc1db9f7f2313c08707
RustinParsen <rustinparsen@gmail.com>
2020-08-06 22:28:22 -0400

    issue #1 fixed

--- CirclesOnTheMove.js
+++ CirclesOnTheMove.js
@@ -1,6 +1,18 @@
 var canvas = document.querySelector("canvas");
-
+var ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-console.log(canvas);
+drawCircle(200,200,200,"red");
+drawCircle(200,400,200,"blue");
+drawCircle(300,200,100,"purple");
+
+
+
+function drawCircle(x,y,r,color){
+    ctx.beginPath();
+    ctx.arc(x,y,r,0,Math.PI*2,false);
+    ctx.strokeStyle = color;
+    ctx.stroke();
+    
+}
