7d20517436a7b54a48d964b56896ff60a9c94e27
RustinParsen <rustinparsen@gmail.com>
2020-08-08 18:35:41 -0400

    bouning ball

--- CirclesOnTheMove.js
+++ CirclesOnTheMove.js
@@ -3,16 +3,36 @@ var ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-drawCircle(200,200,200,"red");
-drawCircle(200,400,200,"blue");
-drawCircle(300,200,100,"purple");
+var x = 100;
+var y = 100;
+var r = 50;
+var dx = 20;
+var dy = 20;
+
+
+
+function animate(){
+    requestAnimationFrame(animate);
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    drawCircle(x,y,r,"red");
+    if(x + r > canvas.width || x - r < 0){
+        dx = -dx;
+    }
+    if(y + r > canvas.height ||  y - r < 0){
+        dy = -dy;
+    }
+    x = x + dx;
+    y = y + dy;
+}
+
+animate();
 
 
 
 function drawCircle(x,y,r,color){
     ctx.beginPath();
     ctx.arc(x,y,r,0,Math.PI*2,false);
-    ctx.strokeStyle = color;
-    ctx.stroke();
+    ctx.fillStyle = color;
+    ctx.fill();
     
 }
